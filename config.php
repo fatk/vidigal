33066e47c7994d85c5a93ad730d92fc2bbcfe2bc
fatk <karl.fathi@gmail.com>
2014-04-10 11:14:59 -0300

    Added icons paths

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
 
 /*
+    Environment Check (This should be define in wp-config.php or wp-local.php)
+*/
+
+if (!defined('_ENVIRONMENT_')) {
+    define('_ENVIRONMENT_', 'dev');
+}
+
+/*
   Paths
 */
 define('_PATH_CONFIG_', 'config');
@@ -8,6 +16,7 @@ define('_PATH_ASSETS_', 'assets/'._ENVIRONMENT_);
 define('_PATH_ASSETS_IMAGES_', _PATH_ASSETS_.'/images');
 define('_PATH_ASSETS_SCRIPTS_', _PATH_ASSETS_.'/scripts');
 define('_PATH_ASSETS_STYLES_', _PATH_ASSETS_.'/styles');
+define('_PATH_ASSETS_ICONS_', _PATH_ASSETS_.'/icons');
 define('_PATH_VIEWS_', 'views');
 
 define('_ABSPATH_CONFIG_', dirname(__FILE__).'/'._PATH_CONFIG_);
@@ -15,6 +24,7 @@ define('_ABSPATH_ASSETS_', dirname(__FILE__).'/'._PATH_ASSETS_);
 define('_ABSPATH_ASSETS_IMAGES_', dirname(__FILE__).'/'._PATH_ASSETS_IMAGES_);
 define('_ABSPATH_ASSETS_SCRIPTS_', dirname(__FILE__).'/'._PATH_ASSETS_SCRIPTS_);
 define('_ABSPATH_ASSETS_STYLES_', dirname(__FILE__).'/'._PATH_ASSETS_STYLES_);
+define('_ABSPATH_ASSETS_ICONS_', dirname(__FILE__).'/'._PATH_ASSETS_ICONS_);
 define('_ABSPATH_VIEWS_', dirname(__FILE__).'/'._PATH_VIEWS_);
 
 define('_URIPATH_CONFIG_', get_template_directory_uri().'/'._PATH_CONFIG_);
@@ -22,6 +32,7 @@ define('_URIPATH_ASSETS_', get_template_directory_uri().'/'._PATH_ASSETS_);
 define('_URIPATH_ASSETS_IMAGES_', get_template_directory_uri().'/'._PATH_ASSETS_IMAGES_);
 define('_URIPATH_ASSETS_SCRIPTS_', get_template_directory_uri().'/'._PATH_ASSETS_SCRIPTS_);
 define('_URIPATH_ASSETS_STYLES_', get_template_directory_uri().'/'._PATH_ASSETS_STYLES_);
+define('_URIPATH_ASSETS_ICONS_', get_template_directory_uri().'/'._PATH_ASSETS_ICONS_);
 define('_URIPATH_VIEWS_', get_template_directory_uri().'/'._PATH_VIEWS_);
 
 /*
